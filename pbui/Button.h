3b5d876d22b10f6a1ec9c61a511dc702f476c5da
Lord Dreadfight <119334716+LordDreadfight@users.noreply.github.com>
2024-11-23 15:39:05 +0100

    changed to structs from classes
    we wouldnt want our c++ to look like c# wouldnt we

--- pbui/Button.h
+++ pbui/Button.h
@@ -3,17 +3,15 @@
 #include <functional>
 #include <string>
 
-class Button : public UIElement {
-public:
+struct Button : public UIElement {
     Button(int x, int y, int width, int height, const std::string& text);
     void setOnClick(std::function<void()> callback);
-
     void draw(SDL_Renderer* renderer) override;
     void handleEvent(const SDL_Event& event) override;
-
-private:
     int x, y, width, height;
     std::string text;
     std::function<void()> onClick;
     bool isHovered;
-};
+}
+
+
